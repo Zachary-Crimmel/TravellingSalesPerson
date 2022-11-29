fab2bf40575697169e8852c8633cf9a4eb271995
Zachary Crimmel <zcrimmel@gmail.com>
2022-11-29 18:51:20 +0000

    Setup framework for heldKarp

---
+++ index.js
@@ -0,0 +1,18 @@
+function heldKarp(cities, start){
+  if(cities.length == 1 || cities.length == 0){
+    console.log("Invalid list of cities, modify and then try again.");
+    return 0;q
+  }
+  if(cities.length == 2){
+    return cities[0][1];
+  }
+  return "Path is more than 3 cities";
+}
+
+var city1 = [
+  [0, 1, 2],
+  [1, 0, 2],
+  [2, 2, 0]
+];
+
+console.log(heldKarp(city1, 10));
