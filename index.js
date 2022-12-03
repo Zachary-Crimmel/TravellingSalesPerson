db7720d80e45b769e353439b7e111e3cefdfbe0a
Zachary-Crimmel <zcrimmel@gmail.com>
2022-12-02 19:36:36 -0700

    Some more work on matrices

--- index.js
+++ index.js
@@ -8,27 +8,52 @@ There is both the original version and a dynamic version of the program.
 Sources:
 - https://www.youtube.com/watch?v=-JjA4BLQyqE
 - https://github.com/mission-peace/interview/blob/master/src/com/interview/graph/TravelingSalesmanHeldKarp.java 
+- https://en.wikipedia.org/wiki/Held%E2%80%93Karp_algorithm#Pseudocode[4] 
+- https://www.youtube.com/watch?v=9DwFwkAwnmY 
+- https://stackoverflow.com/questions/42773836/how-to-find-all-subsets-of-a-set-in-javascript-powerset-of-array 
+- https://www.folkstalk.com/2022/09/javascript-create-array-from-1-to-n-with-code-examples.html 
 */
 
-function getMinDistance(endVertex, subset, matrix) {
+const getAllSubsets =
+  theArray => theArray.reduce(
+    (subsets, value) => subsets.concat(
+      subsets.map(set => [value, ...set])
+    ),
+    [[]]
+  );
+
+function getMinDistance(endVertex, subset, matrix, start) {
   var minDistance = 100000000;
-  if(subset.length == 1){
-    minDistance = 0;
+  if (subset.length == 0) {
+    minDistance = matrix[start][subset[0]];
+    return minDistance;
+  } else if (subset.length == 1) {
+
   }
 }
 
-function heldKarp(cities, start) {
-  var shortestDistance = 0;
-  if (cities.length <= 1) {
+function tsp_hk(distance_matrix) {
+  var cost = 0;
+  var length = distance_matrix.length;
+  var subsets = getAllSubsets(Array.from({ length: length }, (_, i) => i + 1)).filter(s => s.length == length - 1);
+  //console.log(subsets);
+  if (distance_matrix.length <= 1) {
     console.log("Invalid list of cities, modify and then try again.");
     return 0;
   }
-  if (cities.length == 2) {
-    return cities[0][1];
+  if (distance_matrix.length == 2) {
+    return distance_matrix[0][1];
   }
   else {
-    // return the minimum of 
-    // 
+    // for (var i = 0; i < subsets.length; i++) {
+    //   const index = array.indexOf(i);
+    //   if (index > -1) { // only splice array when item is found
+    //     array.splice(index, 1); // 2nd parameter means remove one item only
+    //   }
+    //   return Math.min(getMinDistance)
+    // }
+
+    
   }
 }
 
@@ -51,4 +76,21 @@ var city2 = [
   [10, 4, 8, 0]
 ];
 
-console.log(heldKarp(city1, 0));
+var city3 = [3];
+
+var city4 = [
+  [0, 15],
+  [15, 0]
+];
+
+console.log("Path of minimum cost is:");
+console.log(tsp_hk(city1, 0));
+
+console.log("Path of minimum cost is:");
+console.log(tsp_hk(city2, 0));
+
+console.log("Path of minimum cost is:");
+console.log(tsp_hk(city3, 0));
+
+console.log("Path of minimum cost is:");
+console.log(tsp_hk(city4, 0));
